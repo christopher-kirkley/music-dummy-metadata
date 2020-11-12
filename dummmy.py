04ae9f0dd0b2268bc38f315c01cd8a5fa4dd505e
Christopher Kirkley <ckirkley@gmail.com>
2020-11-12 11:56:31 -0800

    Artist source refactored.

--- dummmy.py
+++ dummmy.py
@@ -9,6 +9,15 @@ fake = Faker()
 catalog_root = 'TR'
 number_of_items = 50
 
+temp = open('./artistnames.txt').read().splitlines()
+artist_source = [name.strip() for name in temp]
+
+"""Choose the artists for this instance."""
+number_of_artists = 10
+artists = [random.choice(artist_source) for artist_name in range(number_of_artists)]
+
+
+
 df = pd.DataFrame(columns=['catalog_number', 'catalog_artist', 'catalog_name'])
 
 def random_artist(number_of_items):
@@ -31,4 +40,5 @@ df['catalog_name']= random_catalog_name(number_of_items)
 
 print(df)
 
+df = pd.DataFrame(columns=['isrc', 'track_artist', 'track_number', 'catalog_number'])
 
