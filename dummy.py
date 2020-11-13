30a35f1fd708a5bcb2873041bd7c3adc3a78874f
Mako <ckirkley@gmail.com>
2020-11-13 00:52:02 -0800

    Can add various artists as catalog artists.

--- dummy.py
+++ dummy.py
@@ -15,11 +15,17 @@ def make_artist_list():
     return artist_source
 
 
-df = pd.DataFrame(columns=['catalog_number', 'catalog_artist', 'catalog_name'])
 
 def select_artists(number_of_items):
     lines = open('./artistnames.txt').read().splitlines()
     artists = [random.choice(lines).strip() for name in range(number_of_items)]
+    return artists
+
+def select_catalog_artists(size, size_va):
+    artists = select_artists(size)
+    for i in range(size_va):
+        index = random.randint(0, size-1)
+        artists[index] = 'Various Artists'
     return artists
 
 def select_catalog(number_of_items):
@@ -31,11 +37,16 @@ def make_catalog_numbers(catalog_root, number_of_items):
     catalog_numbers = [f'{catalog_root}-00{i}' for i in range(1, number_of_items+1)]
     return catalog_numbers
 
-def main():
-    df['catalog_number'] = generate_catalog_number(number_of_items)
-    df['catalog_artist']= random_artist(number_of_items)
-    df['catalog_name']= random_catalog_name(number_of_items)
+def make_catalog_df(size, size_va):
+    df = pd.DataFrame(columns=['catalog_number', 'catalog_artist', 'catalog_name'])
+    df['catalog_number'] = make_catalog_numbers(catalog_root, size)
+    df['catalog_artist']= select_catalog_artists(size, size_va)
+    df['catalog_name']= select_catalog(size)
+    return df
 
+
+def main():
+    df = make_catalog_df(5, 3)
     print(df)
 
     df = pd.DataFrame(columns=['isrc', 'track_artist', 'track_number', 'catalog_number'])
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from dummy import make_artist_list, select_artists, select_catalog, make_catalog_numbers
+from dummy import make_artist_list, select_artists, select_catalog, make_catalog_numbers, make_catalog_df, select_catalog_artists
 
 
 def test_pytest_working():
@@ -24,3 +24,27 @@ def test_create_catalog_numbers():
     catalog_numbers = make_catalog_numbers(root, size)
     assert len(catalog_numbers) > 0
     assert catalog_numbers[0] == 'TR-001'
+
+def test_can_make_catalog_df():
+    size = 5
+    size_va = 0
+    df = make_catalog_df(size, size_va)
+    assert len(df) == 5
+    assert df['catalog_number'][0] == 'TR-001'
+    assert type(df['catalog_artist'][0]) == str
+    assert type(df['catalog_name'][0]) == str
+
+def test_can_select_catalog_artists():
+    size = 5
+    artists = select_artists(size)
+
+    def va_list(artists):
+        res = []
+        for index, key in enumerate(artists):
+            if artists[index] == 'Various Artists':
+                res.append(index)
+        return res
+    
+    artists = select_catalog_artists(5, 1)
+    assert len(artists) == 5
+    assert len(va_list(artists)) == 1
