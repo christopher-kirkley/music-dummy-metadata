d991c4e1a1e9a454cc9a20c438d42ccede4cbd53
Mako <ckirkley@gmail.com>
2020-11-17 23:02:44 -0800

    Started refactoring.

--- dummy.py
+++ dummy.py
@@ -3,63 +3,49 @@ import random
 import numpy as np
 import pandas as pd
 
+from collections import defaultdict
+
 from faker import Faker
 fake = Faker()
 
 catalog_root = 'TR'
 number_of_items = 50
 
-class Artists():
-    def __init__(self, number_of_artists):
-        self.number_of_artists = number_of_artists
-
-    def make_artist_list(self):
-        temp = open('./artistnames.txt').read().splitlines()
-        artist_source = [name.strip() for name in temp]
-        return artist_source
-
-def select_artists(number_of_items):
-    lines = open('./artistnames.txt').read().splitlines()
-    artists = [random.choice(lines).strip() for name in range(number_of_items)]
-    return artists
-
-
 class Catalogs():
-    def __init__(self, catalog_root, number_of_items, size_va):
+    def __init__(self, catalog_root, number_of_catalogs, size_va):
         self.catalog_root = catalog_root
-        self.number_of_items = number_of_items
+        self.number_of_catalogs = number_of_catalogs
+        self.size_va = size_va
 
     def select_catalog_artists(self):
-        artists = select_artists(self.number_of_items)
+        artists = make_artist_list(self.number_of_catalogs)
         for i in range(self.size_va):
             artists[i] = 'Various Artists'
         return artists
 
     def select_catalog(self):
         lines = open('./artistnames.txt').read().splitlines()
-        catalog_names = [random.choice(lines).strip() for name in range(self.number_of_items)]
+        catalog_names = [random.choice(lines).strip() for name in range(self.number_of_catalogs)]
         return catalog_names
 
     def make_catalog_numbers(self):
-        catalog_numbers = [f'{self.catalog_root}-00{i}' for i in range(1, self.number_of_items+1)]
+        catalog_numbers = [f'{self.catalog_root}-00{i}' for i in range(1, self.number_of_catalogs+1)]
         return catalog_numbers
 
-    def make_catalog_df(self):
-        df = pd.DataFrame(columns=['catalog_number', 'catalog_artist', 'catalog_name'])
-        df['catalog_number'] = self.make_catalog_numbers()
-        df['catalog_artist']= self.select_catalog_artists()
-        df['catalog_name']= self.select_catalog()
-        return df
+def make_artist_list(number_of_items):
+    lines = open('./artistnames.txt').read().splitlines()
+    artist_list = [random.choice(lines).strip() for name in range(number_of_items)]
+    return artist_list
+
+def random_phrase(artist_list):
+    index = random.randint(1, 9)
+    phrase = artist_list[index]
+    return phrase
 
 
-
-def make_track_artist():
-    artist = select_artists(1)[0]
-    return artist
-
-def make_track_title():
-    title = select_catalog(1)[0]
-    return title
+# def find_unique_catalog_numbers(catalog_df):
+#     catalog_numbers = catalog_df['catalog_number'].unique().tolist()
+#     return catalog_numbers
 
 def make_track_isrcs(catalog_number):
     tracks_per_catalog = random.randint(2, 10)
@@ -68,69 +54,106 @@ def make_track_isrcs(catalog_number):
         isrcs.append(f'US12319{catalog_number[-3:]}{i+1:02d}')
     return isrcs
 
-def find_unique_catalog_numbers(catalog_df):
-    catalog_numbers = catalog_df['catalog_number'].unique().tolist()
-    return catalog_numbers
+def make_df(catalogs):
+    artist_list= make_artist_list(10)
+    catalog_numbers = catalogs.make_catalog_numbers()
+    data = {
+            'isrc': [],
+            'track_artist': [],
+            'track_title': [],
+            'catalog_number':[],
+            'catalog_artist': [],
+            'catalog_name': []
+            }
+    for catalog_number in catalog_numbers:
+        isrcs = make_track_isrcs(catalog_number)
+        for isrc in isrcs:
+            data['isrc'].append(isrc)
+            data['track_artist'].append(random_phrase(artist_list))
+            data['track_title'].append('cheese')
+            data['catalog_number'].append(catalog_number)
+            data['catalog_artist'].append('3')
+            data['catalog_name'].append('name')
+    df = pd.DataFrame(data)
+    return df
+        
 
-class tracks:
+
+#     catalog_numbers = find_unique_catalog_numbers(catalog_df)
+#     for catalog_number in catalog_numbers:
+
+        # new_df = pd.DataFrame(columns=['isrc', 'track_artist', 'track_number', 'track_title', 'catalog_number'])
+        # new_df['isrc'] = make_track_isrcs(catalog_number)
+        # new_df['catalog_number'] = catalog_number
+        # new_df['track_artist'] = make_track_artist()
+        # catalog_artist = catalog_df.loc[catalog_df['catalog_number'] == catalog_number]['catalog_artist'].tolist()[0]
+        # if catalog_artist == 'Various Artists':
+        #     new_df['track_artist'] = make_track_artist()
+        # else:
+        #     new_df['track_artist'] = catalog_artist
+        # for index, row in enumerate(new_df.iterrows()):
+        #     new_df['track_number'][index] = index + 1
+        #     new_df['track_title'][index] = make_track_title()
+        # df = df.append(new_df, ignore_index=True)
+    
+
+
+
+
+
+class Tracks():
     def __init__(self):
         pass
 
-    def make_df(self):
-        pass
+
+# def make_track_artist():
+#     artist = select_artists(1)[0]
+#     return artist
+
+# def make_track_title():
+#     title = select_catalog(1)[0]
+#     return title
 
 
-def make_track_df(catalog_df):
-    df = pd.DataFrame(columns=['isrc', 'track_number', 'track_artist', 'track_title', 'catalog_number'])
-    catalog_numbers = find_unique_catalog_numbers(catalog_df)
-    for catalog_number in catalog_numbers:
-        new_df = pd.DataFrame(columns=['isrc', 'track_artist', 'track_number', 'track_title', 'catalog_number'])
-        new_df['isrc'] = make_track_isrcs(catalog_number)
-        new_df['catalog_number'] = catalog_number
-        new_df['track_artist'] = make_track_artist()
-        catalog_artist = catalog_df.loc[catalog_df['catalog_number'] == catalog_number]['catalog_artist'].tolist()[0]
-        if catalog_artist == 'Various Artists':
-            new_df['track_artist'] = make_track_artist()
-        else:
-            new_df['track_artist'] = catalog_artist
-        for index, row in enumerate(new_df.iterrows()):
-            new_df['track_number'][index] = index + 1
-            new_df['track_title'][index] = make_track_title()
-        df = df.append(new_df, ignore_index=True)
-
-    return df
-
-def make_upc():
-    upc = random.randint(10**(12-1), 10**12)
-    return upc
-
-def make_version_numbers(catalog_number):
-    version_numbers = []
-    types = ['dig', 'lp', 'cd', 'cass']
-    for type in types:
-        version_number = f'{catalog_number}{type}'
-        version_numbers.append(version_number)
-    return version_numbers
 
 
-def make_version_df(catalog_df):
-    df = pd.DataFrame(columns=['upc', 'version_number', 'format', 'version_title', 'catalog_number'])
-    catalog_numbers = find_unique_catalog_numbers(catalog_df)
-    for catalog_number in catalog_numbers:
-        new_df = pd.DataFrame(columns=['upc', 'version_number', 'format', 'version_title', 'catalog_number'])
-        new_df['version_number'] = make_version_numbers(catalog_number)
-        new_df['upc'] = make_upc()
-        new_df['catalog_number'] = catalog_number
-        df = df.append(new_df, ignore_index=True)
-    return df
+
+# def make_track_df(catalog_df):
+#     df = pd.DataFrame(columns=['isrc', 'track_number', 'track_artist', 'track_title', 'catalog_number'])
+
+#     return df
+
+# def make_upc():
+#     upc = random.randint(10**(12-1), 10**12)
+#     return upc
+
+# def make_version_numbers(catalog_number):
+#     version_numbers = []
+#     types = ['dig', 'lp', 'cd', 'cass']
+#     for type in types:
+#         version_number = f'{catalog_number}{type}'
+#         version_numbers.append(version_number)
+#     return version_numbers
+
+
+# def make_version_df(catalog_df):
+#     df = pd.DataFrame(columns=['upc', 'version_number', 'format', 'version_title', 'catalog_number'])
+#     catalog_numbers = find_unique_catalog_numbers(catalog_df)
+#     for catalog_number in catalog_numbers:
+#         new_df = pd.DataFrame(columns=['upc', 'version_number', 'format', 'version_title', 'catalog_number'])
+#         new_df['version_number'] = make_version_numbers(catalog_number)
+#         new_df['upc'] = make_upc()
+#         new_df['catalog_number'] = catalog_number
+#         df = df.append(new_df, ignore_index=True)
+#     return df
 
 
 
 def main():
-    catalog_df = make_catalog_df(5, 3)
-    catalog_df.to_csv('catalog.csv')
-    track_df = make_track_df(catalog_df)
-    track_df.to_csv('track.csv')
+    catalogs = Catalogs('TR', 5, 2)
+    df = make_df(catalogs)
+    print(df)
+
 
 
 
---
+++ test_df.py
@@ -0,0 +1,110 @@
+from dummy import Catalogs, make_artist_list, make_df
+
+
+def test_pytest_working():
+    assert True
+
+def test_can_select_artists():
+    artists = make_artist_list(10)
+    assert len(artists) == 10
+
+def test_get_selection_of_catalog():
+    catalogs = Catalogs('TR', 5, 1)
+    catalog_selection = catalogs.select_catalog()
+    assert len(catalog_selection) == 5
+
+def test_create_catalog_numbers():
+    catalogs = Catalogs('TR', 5, 1)
+    catalog_numbers = catalogs.make_catalog_numbers()
+    assert len(catalog_numbers) > 0
+    assert catalog_numbers[0] == 'TR-001'
+
+def test_can_make_catalog_df():
+    catalogs = Catalogs('TR', 5, 1)
+    df = make_df(catalogs)
+    artist_list = make_artist_list(10)
+    assert len(df) > 5
+    assert df['catalog_number'][0] == 'TR-001'
+    assert df['isrc'][0] == 'US1231900101'
+    assert type(df['track_artist'][0]) == str
+    assert type(df['track_title'][0]) == str
+    assert type(df['catalog_artist'][0]) == str
+    assert type(df['catalog_name'][0]) == str
+
+# def test_can_select_catalog_artists():
+#     size = 5
+#     artists = select_artists(size)
+
+#     def indices_of_various_artists(artists):
+#         res = []
+#         for index, key in enumerate(artists):
+#             if artists[index] == 'Various Artists':
+#                 res.append(index)
+#         return res
+    
+#     artists = select_catalog_artists(5, 1)
+#     assert len(artists) == 5
+#     assert len(indices_of_various_artists(artists)) == 1
+
+#     artists = select_catalog_artists(5, 2)
+#     assert len(artists) == 5
+#     assert len(indices_of_various_artists(artists)) == 2
+
+#     artists = select_catalog_artists(5, 3)
+#     assert len(artists) == 5
+#     assert len(indices_of_various_artists(artists)) == 3
+
+
+# def test_can_make_track_isrcs():
+#     isrcs = make_track_isrcs('TR-001')
+#     assert len(isrcs) > 0
+#     assert len(isrcs[0]) == 12
+#     assert isrcs[0] == 'US1231900101'
+#     assert isrcs[1] == 'US1231900102'
+
+
+# def test_can_find_unique_catalog_numbers():
+#     catalog_df = make_catalog_df(5, 0)
+#     catalog_numbers = find_unique_catalog_numbers(catalog_df)
+#     assert len(catalog_numbers) > 0
+
+# def test_can_make_tracks_df():
+#     catalog_df = make_catalog_df(5, 1)
+#     df = make_track_df(catalog_df)
+#     assert len(df) > 0
+#     assert df['isrc'][0] == 'US1231900101'
+#     assert df['catalog_number'][0] == 'TR-001'
+#     assert df['track_number'][0] == 1
+#     assert type(df['track_title'][0]) == str
+#     assert df['track_number'][1] == 2
+#     assert type(df['track_artist'][0]) == str
+#     assert df['isrc'][1] == 'US1231900102'
+#     if catalog_df['catalog_artist'][0] != 'Various Artists':
+#         assert catalog_df['catalog_artist'][0] == df['track_artist'][0]
+#     if catalog_df['catalog_artist'][0] == 'Various Artists':
+#         assert str(df['track_artist'][0])
+#         assert df['track_artist'][0] != 'Various Artists' 
+
+# def test_can_make_track_artist():
+#     artist = make_track_artist()
+#     assert type(artist) == str
+
+# def test_can_make_track_title():
+#     title = make_track_title()
+#     assert type(title) == str
+
+# def test_can_make_version_df():
+#     catalog_df = make_catalog_df(5, 0)
+#     assert len(catalog_df) > 0
+#     df = make_version_df(catalog_df)
+#     assert len(df) > 0
+#     assert type(df['upc'][0]) == int
+
+# def test_can_make_upc():
+#     upc = make_upc()
+#     assert len(str(upc)) == 12
+    
+# def test_can_make_version_numbers():
+#     catalog_number = 'TR-001'
+#     version_numbers = make_version_numbers(catalog_number)
+#     assert len(version_numbers) > 0
