35caf0b1a1126674b90ea099fae78025807614ff
Mako <ckirkley@gmail.com>
2020-11-19 15:13:24 -0800

    Add function to clean personal info from df.

--- clean.py
+++ clean.py
@@ -22,13 +22,21 @@ def fake_name():
     last_name = fake.last_name_nonbinary()
     return first_name + ' ' + last_name
 
-def clean_names(df, indexes):
+def change_info(df, indexes):
+    new_name = fake_name()
     for index in indexes:
-        df.at[index, 'buyer name'] = fake_name()
+        df.at[index, 'buyer name'] = new_name
         df.at[index, 'buyer email'] = fake.ascii_email()
         df.at[index, 'buyer phone'] = fake.phone_number()
         df.at[index, 'ship to name'] = fake_name()
         df.at[index, 'ship to street'] = fake.street_address()
+    return df
+
+def clean_names(df):
+    buyer_names = find_unique_buyers(df)
+    for buyer_name in buyer_names:
+        indexes = find_indexes_of_buyer(df, buyer_name)
+        df = change_info(df, indexes)
     return df
 
 
@@ -36,10 +44,6 @@ def clean_names(df, indexes):
 
 def main():
     df = import_csv()
-    buyer_names = find_unique_buyers(df)
-    for buyer_name in buyer_names:
-        indexes = find_indexes_of_buyer(df, buyer_name)
-        df = clean_names(df, indexes)
     df.to_csv('out.csv')
 
 
--- test_bandcamp.py
+++ test_bandcamp.py
@@ -1,6 +1,6 @@
 import os
 
-from clean import import_csv, clean_names, find_unique_buyers, find_indexes_of_buyer
+from clean import import_csv, clean_names, find_unique_buyers, find_indexes_of_buyer, change_info
 
 def test_true():
     assert True
@@ -32,11 +32,21 @@ def test_can_change_buyer_info():
     assert df.loc[indexes[0]]['buyer phone'] == 'oldphone'
     assert df.loc[indexes[0]]['ship to name'] == 'Lou Bob'
     assert df.loc[indexes[0]]['ship to street'] == 'oldaddress'
-    df = clean_names(df, indexes)
+    df = change_info(df, indexes)
     assert len(df) > 0
     assert type(df.loc[indexes[0]]['buyer name']) == str
     assert df.loc[indexes[0]]['buyer email'] != 'oldemail@gmail.com'
     assert df.loc[indexes[0]]['buyer phone'] != 'oldphone'
     assert df.loc[indexes[0]]['ship to name'] != 'Lou Bob'
     assert df.loc[indexes[0]]['ship to street'] != 'oldaddress'
+
+def test_can_clean_names():
+    df = import_csv()
+    df = clean_names(df)
+    assert df.loc[1]['buyer name'] != 'Lou Bob'
+    assert df.loc[1]['buyer email'] != 'oldemail@gmail.com'
+    assert df.loc[1]['buyer phone'] != 'oldphone'
+    assert df.loc[1]['ship to name'] != 'Lou Bob'
+    assert df.loc[1]['ship to street'] != 'oldaddress'
+
 
