a83e88584b65c6c155f5f9327711a149c087f0d3
Mako <ckirkley@gmail.com>
2020-11-22 08:45:37 -0800

    Can clean physical item data

--- clean.py
+++ clean.py
@@ -53,7 +53,12 @@ class Bandcamp_df():
     def change_physical_item(self, indexes):
         for index in indexes:
             catalog_item = random.choice(self.catalog_info)
-            self.df.at[index, 'catalog_number'] = catalog_item['catalog_number']
+            self.df.at[index, 'catalog number'] = catalog_item['catalog_number']
+            self.df.at[index, 'item name'] = catalog_item['catalog_name']
+            self.df.at[index, 'artist'] = catalog_item['catalog_artist']
+            self.df.at[index, 'sku'] = catalog_item['version_number']
+            self.df.at[index, 'upc'] = catalog_item['upc']
+            self.df.at[index, 'item url'] = ''
         return self.df
 
 
@@ -74,10 +79,33 @@ class Catalog():
     def find_catalog_info(self):
         catalog_info = []
         for catalog_number in self.catalog_selection():
-            entry = {'catalog_number': catalog_number}
+            version_info = self.find_version_info(catalog_number)
+            key = random.randint(0,3)
+            entry = {
+                    'catalog_number': catalog_number,
+                    'catalog_name': version_info['catalog_name'][key],
+                    'catalog_artist': version_info['catalog_artist'][key],
+                    'version_number': version_info['version_number'][key],
+                    'upc': version_info['upc'][key],
+                    }
             catalog_info.append(entry)
         return catalog_info
 
+    def find_version_info(self, catalog_number):
+        version_info = []
+        new_catalog_df = self.catalog_df.copy()
+        new_catalog_df.drop(
+                [
+                'track_artist',
+                'track_title',
+                'isrc'],
+                axis=1,
+                inplace=True
+                )
+        joined_df = new_catalog_df.merge(self.version_df, on='catalog_number')
+        version_df_selection = joined_df.loc[joined_df['catalog_number'] == catalog_number]
+        version_df_selection.drop_duplicates(inplace=True)
+        return version_df_selection.to_dict('list')
 
 
 def load_catalog():
--- test_bandcamp.py
+++ test_bandcamp.py
@@ -97,13 +97,24 @@ def test_can_load_version():
     assert len(version_df) > 0
 
 def test_can_change_physical_item():
-    catalog_info = [{'catalog_number': 'TR-001'}]
+    catalog_info = [
+            {
+                'catalog_number': 'TR-001',
+                'catalog_name': 'Bobo',
+                'catalog_artist': 'Tomato Jones',
+                'version_number': 'TR-001lp',
+                'upc': '1234',
+                }]
     df = make_df()
     bandcamp = Bandcamp_df(df, catalog_info)
     indexes = bandcamp.physical_indexes()
     df = bandcamp.change_physical_item(indexes)
-    assert df.loc[1]['catalog_number'] == 'TR-001'
-    assert df.loc[1]['item name'] == 'TR-00'
+    assert df.loc[1]['catalog number'] == 'TR-001'
+    assert df.loc[1]['item name'] == 'Bobo'
+    assert df.loc[1]['artist'] == 'Tomato Jones'
+    assert df.loc[1]['sku'] == 'TR-001lp'
+    assert df.loc[1]['upc'] == '1234'
+    assert df.loc[1]['item url'] == ''
 
 def test_can_get_catalog_selection():
     catalog_df = load_catalog()
@@ -118,4 +129,27 @@ def test_can_find_catalog_info():
     catalog_info = catalog.find_catalog_info()
     assert len(catalog_info) == 3
 
+def test_can_change_physical_item_functional():
+    catalog_df = load_catalog()
+    version_df = load_version()
+    catalog = Catalog(catalog_df, version_df, 1)
+    catalog_info = catalog.find_catalog_info()
+    df = make_df()
+    bandcamp = Bandcamp_df(df, catalog_info)
+    indexes = bandcamp.physical_indexes()
+    df = bandcamp.change_physical_item(indexes)
+    assert df.loc[1]['catalog number'].startswith('TR-00')
+    assert type(df.loc[1]['item name']) == str
+    assert type(df.loc[1]['artist']) == str
+    assert df.loc[1]['sku'].startswith('TR-00')
+    assert type(df.loc[1]['upc']) == int
+    assert df.loc[1]['item url'] == ''
+
+def test_can_make_catalog_entry_df():
+    catalog_df = load_catalog()
+    version_df = load_version()
+    catalog = Catalog(catalog_df, version_df, 3)
+    catalog_number = 'TR-001'
+    version_info = catalog.find_version_info(catalog_number)
+    assert len(version_info) > 0
 
