0413837f54bece458fc0ea08ef99bad431e986d6
Christopher Kirkley <ckirkley@gmail.com>
2020-11-25 09:34:27 -0800

    Refactored to use json catalog info.

--- clean.py
+++ clean.py
@@ -17,9 +17,9 @@ def fake_name():
     return first_name + ' ' + last_name
 
 class Bandcamp_df():
-    def __init__(self, df, catalog_info):
+    def __init__(self, df, catalog_json):
         self.df = df
-        self.catalog_info = catalog_info
+        self.catalog_json = catalog_json
 
     def find_unique_buyers(self):
         buyer_names = self.df['buyer name'].unique().tolist()
@@ -53,86 +53,24 @@ class Bandcamp_df():
     def change_physical_item(self, indexes):
         for index in indexes:
             key = random.randint(0, 3)
-            catalog_item = random.choice(self.catalog_info)
+            catalog_item = random.choice(self.catalog_json)
             self.df.at[index, 'catalog number'] = catalog_item['catalog_number']
             self.df.at[index, 'item name'] = catalog_item['catalog_name']
             self.df.at[index, 'artist'] = catalog_item['catalog_artist']
-            self.df.at[index, 'sku'] = catalog_item['version'][key]['version_number']
-            self.df.at[index, 'upc'] = catalog_item['version'][key]['upc']
+            self.df.at[index, 'sku'] = catalog_item['versions'][key]['versions_number']
+            self.df.at[index, 'upc'] = catalog_item['versions'][key]['upc']
             self.df.at[index, 'item url'] = ''
         return self.df
 
-
-class Catalog():
-    def __init__(self, catalog_df, version_df, number_of_items):
-        self.catalog_df = catalog_df
-        self.version_df = version_df
-        self.number_of_items = number_of_items
-
-    def catalog_selection(self):
-        catalog_ids = self.catalog_df['catalog_number'].unique().tolist()
-        catalog_selection = []
-        for i in range(self.number_of_items):
-            index = random.randint(0, self.number_of_items)
-            catalog_selection.append(catalog_ids[index])
-        return catalog_selection
-
-    def find_catalog_info(self):
-        catalog_info = []
-        for catalog_number in self.catalog_selection():
-            version_info = self.find_version_info(catalog_number)
-            key = random.randint(0,3)
-            version = []
-            for index, info in enumerate(version_info['version_number']):
-                version.append(
-                        {
-                            'version_number': info,
-                            'upc': version_info['upc'][index],
-                            })
-            entry = {
-                    'catalog_number': catalog_number,
-                    'catalog_name': version_info['catalog_name'][key],
-                    'catalog_artist': version_info['catalog_artist'][key],
-                    'version': version
-                    }
-            catalog_info.append(entry)
-        return catalog_info
-
-    def find_version_info(self, catalog_number):
-        version_info = []
-        new_catalog_df = self.catalog_df.copy()
-        new_catalog_df.drop(
-                [
-                'track_artist',
-                'track_title',
-                'isrc'],
-                axis=1,
-                inplace=True
-                )
-        joined_df = new_catalog_df.merge(self.version_df, on='catalog_number')
-        version_df_selection = joined_df.loc[joined_df['catalog_number'] == catalog_number]
-        version_df_selection.drop_duplicates(inplace=True)
-        return version_df_selection.to_dict('list')
-
-
-def load_catalog():
-    path = os.getcwd() + '/catalog.csv'
-    df = pd.read_csv(path, encoding='utf-8')
-    return df
-
-def load_version():
-    path = os.getcwd() + '/version.csv'
-    df = pd.read_csv(path, encoding='utf-8')
-    return df
 
 
 def main():
     catalog_df = load_catalog()
     version_df = load_version()
     catalog = Catalog(catalog_df, version_df, 3)
-    catalog_info = catalog.find_catalog_info()
+    catalog_json = catalog.find_catalog_json()
     df = make_df()
-    bandcamp = Bandcamp_df(df, catalog_info)
+    bandcamp = Bandcamp_df(df, catalog_json)
     indexes = bandcamp.physical_indexes()
     df = bandcamp.change_physical_item(indexes)
 
--- test_bandcamp.py
+++ test_bandcamp.py
@@ -1,77 +1,73 @@
 import os
 
-from clean import load_catalog, load_version, make_df
+import pytest
 
-from clean import Bandcamp_df, Catalog
+from clean import Bandcamp_df, make_df
 
-def make_test_json():
-    catalog_json = {
-            'catalog_number': 'TR-001',
-            'catalog_name': 'Cheeseburger',
-            'catalog_artist': 'Stevie B.',
-            'versions': [
-                {
-                    'version_number': 'TR-001lp',
-                    'version_name': 'Vinyl',
-                    'upc': '1234567',
-                    },
-                ],
-            'tracks': [
-                {
-                    'track_number': 1,
-                    'track_artist': 'Stevie B.',
-                    'track_name': 'Babaloo',
-                    'isrc': 'QA123',
-                    },
-                ]
-                }
+def make_catalog_json():
+    catalog_json = [
+            {
+                'catalog_number': 'TR-001',
+                'catalog_name': 'Cheeseburger',
+                'catalog_artist': 'Stevie B.',
+                'versions': [
+                    {
+                        'version_number': 'TR-001lp',
+                        'version_name': 'Vinyl',
+                        'upc': '1234567',
+                        },
+                    ],
+                'tracks': [
+                    {
+                        'track_number': 1,
+                        'track_artist': 'Stevie B.',
+                        'track_name': 'Babaloo',
+                        'isrc': 'QA123',
+                        },
+                    ]
+                    }
+            ]
+    return catalog_json
+
+@pytest.fixture
+def bandcamp_df():
+    catalog_json = make_catalog_json()
+    df = make_df()
+    bandcamp_df = Bandcamp_df(df, catalog_json)
+    yield bandcamp_df
 
 def test_can_create_df():
     df = make_df()
     assert len(df) > 0
 
 def test_can_initialize_bandcamp_df():
-    catalog_df = load_catalog()
-    version_df = load_version()
-    catalog = Catalog(catalog_df, version_df, 3)
-    catalog_info = catalog.find_catalog_info()
+    catalog_json = make_catalog_json()
     df = make_df()
-    bandcamp = Bandcamp_df(df, catalog_info)
-    assert len(df) == len(bandcamp.df)
+    bandcamp_df = Bandcamp_df(df, catalog_json)
+    assert len(df) == len(bandcamp_df.df)
+    assert len(bandcamp_df.catalog_json) > 0
 
-def test_can_clean_names():
-    buyer_names = bandcamp.find_unique_buyers(df)
+def test_can_clean_names(bandcamp_df):
+    buyer_names = bandcamp_df.find_unique_buyers()
     assert len(buyer_names) > 0
 
-def test_can_find_index_of_buyer():
-    catalog_df = load_catalog()
-    version_df = load_version()
-    catalog = Catalog(catalog_df, version_df, 3)
-    catalog_info = catalog.find_catalog_info()
-    df = make_df()
-    bandcamp = Bandcamp_df(df, catalog_info)
+def test_can_find_index_of_buyer(bandcamp_df):
     buyer_name = 'Lou Bob'
-    indexes = bandcamp.buyer_indexes(buyer_name)
+    indexes = bandcamp_df.buyer_indexes(buyer_name)
     assert len(indexes) == 3
     assert indexes[0] == 1
     assert indexes[1] == 2
-    assert df.loc[indexes[0]]['buyer name'] == 'Lou Bob'
-    assert df.loc[indexes[1]]['buyer name'] == 'Lou Bob'
+    assert bandcamp_df.df.loc[indexes[0]]['buyer name'] == 'Lou Bob'
+    assert bandcamp_df.df.loc[indexes[1]]['buyer name'] == 'Lou Bob'
 
-def test_can_find_indexes_of_buyer():
-    catalog_df = load_catalog()
-    version_df = load_version()
-    catalog = Catalog(catalog_df, version_df, 3)
-    catalog_info = catalog.find_catalog_info()
-    df = make_df()
-    bandcamp = Bandcamp_df(df, catalog_info)
+def test_can_find_indexes_of_buyer(bandcamp_df):
     buyer_name = 'Lou Bob'
-    indexes = bandcamp.buyer_indexes(buyer_name)
-    assert df.loc[indexes[0]]['buyer email'] == 'oldemail@gmail.com'
-    assert df.loc[indexes[0]]['buyer phone'] == 'oldphone'
-    assert df.loc[indexes[0]]['ship to name'] == 'Lou Bob'
-    assert df.loc[indexes[0]]['ship to street'] == 'oldaddress'
-    df = bandcamp.change_info(indexes)
+    indexes = bandcamp_df.buyer_indexes(buyer_name)
+    assert bandcamp_df.df.loc[indexes[0]]['buyer email'] == 'oldemail@gmail.com'
+    assert bandcamp_df.df.loc[indexes[0]]['buyer phone'] == 'oldphone'
+    assert bandcamp_df.df.loc[indexes[0]]['ship to name'] == 'Lou Bob'
+    assert bandcamp_df.df.loc[indexes[0]]['ship to street'] == 'oldaddress'
+    df = bandcamp_df.change_info(indexes)
     assert len(df) > 0
     assert type(df.loc[indexes[0]]['buyer name']) == str
     assert df.loc[indexes[0]]['buyer email'] != 'oldemail@gmail.com'
@@ -79,102 +75,36 @@ def test_can_find_indexes_of_buyer():
     assert df.loc[indexes[0]]['ship to name'] != 'Lou Bob'
     assert df.loc[indexes[0]]['ship to street'] != 'oldaddress'
 
-def test_can_clean_names():
-    catalog_df = load_catalog()
-    version_df = load_version()
-    catalog = Catalog(catalog_df, version_df, 3)
-    catalog_info = catalog.find_catalog_info()
-    df = make_df()
-    bandcamp = Bandcamp_df(df, catalog_info)
-    df = bandcamp.clean_names()
+def test_can_clean_two(bandcamp_df):
+    df = bandcamp_df.clean_names()
     assert df.loc[1]['buyer name'] != 'Lou Bob'
     assert df.loc[1]['buyer email'] != 'oldemail@gmail.com'
     assert df.loc[1]['buyer phone'] != 'oldphone'
     assert df.loc[1]['ship to name'] != 'Lou Bob'
     assert df.loc[1]['ship to street'] != 'oldaddress'
 
-def test_can_find_indexes_of_physical_items():
-    catalog_df = load_catalog()
-    version_df = load_version()
-    catalog = Catalog(catalog_df, version_df, 3)
-    catalog_info = catalog.find_catalog_info()
-    df = make_df()
-    bandcamp = Bandcamp_df(df, catalog_info)
-    indexes = bandcamp.physical_indexes()
+def test_can_find_indexes_of_physical_items(bandcamp_df):
+    indexes = bandcamp_df.physical_indexes()
     assert len(indexes) > 0
     assert indexes[0] == 1
     assert indexes[1] == 5
-    assert df.loc[indexes[0]]['item name'] == 'Eghass Malan'
-    assert df.loc[indexes[1]]['item name'] == 'Anou Malane'
+    assert bandcamp_df.df.loc[indexes[0]]['item name'] == 'Eghass Malan'
+    assert bandcamp_df.df.loc[indexes[1]]['item name'] == 'Anou Malane'
 
-def test_can_load_catalog():
-    catalog_df = load_catalog()
-    assert len(catalog_df) > 0
+def test_can_change_physical_item(bandcamp_df):
+    indexes = bandcamp_df.physical_indexes()
+    assert len(indexes) > 0
+    bandcamp_df.change_physical_item(indexes)
+    assert bandcamp_df.df.loc[1]['catalog number'] == 'TR-001'
+    assert bandcamp_df.df.loc[1]['item name'] == 'Bobo'
+    assert bandcamp_df.df.loc[1]['artist'] == 'Tomato Jones'
+    assert bandcamp_df.df.loc[1]['sku'] == 'TR-001lp'
+    assert bandcamp_df.df.loc[1]['upc'] == '1234'
+    assert bandcamp_df.df.loc[1]['item url'] == ''
 
-def test_can_load_version():
-    version_df = load_version()
-    assert len(version_df) > 0
-
-def test_can_change_physical_item():
-    catalog_info = [
-            {
-                'catalog_number': 'TR-001',
-                'catalog_name': 'Bobo',
-                'catalog_artist': 'Tomato Jones',
-                'version': [
-                    {
-                        'version_number': 'TR-001lp',
-                        'upc': '1234',
-                        },
-                    {
-                        'version_number': 'TR-001lp',
-                        'upc': '1234',
-                        },
-                    {
-                        'version_number': 'TR-001lp',
-                        'upc': '1234',
-                        },
-                    {
-                        'version_number': 'TR-001lp',
-                        'upc': '1234',
-                        },
-                    ]
-                }
-                    
-                ]
-    df = make_df()
-    bandcamp = Bandcamp_df(df, catalog_info)
-    indexes = bandcamp.physical_indexes()
-    df = bandcamp.change_physical_item(indexes)
-    assert df.loc[1]['catalog number'] == 'TR-001'
-    assert df.loc[1]['item name'] == 'Bobo'
-    assert df.loc[1]['artist'] == 'Tomato Jones'
-    assert df.loc[1]['sku'] == 'TR-001lp'
-    assert df.loc[1]['upc'] == '1234'
-    assert df.loc[1]['item url'] == ''
-
-def test_can_get_catalog_selection():
-    catalog_df = load_catalog()
-    version_df = load_version()
-    catalog = Catalog(catalog_df, version_df, 3)
-    assert len(catalog.catalog_selection()) == 3
-    
-def test_can_find_catalog_info():
-    catalog_df = load_catalog()
-    version_df = load_version()
-    catalog = Catalog(catalog_df, version_df, 3)
-    catalog_info = catalog.find_catalog_info()
-    assert len(catalog_info) == 3
-
-def test_can_change_physical_item_functional():
-    catalog_df = load_catalog()
-    version_df = load_version()
-    catalog = Catalog(catalog_df, version_df, 1)
-    catalog_info = catalog.find_catalog_info()
-    df = make_df()
-    bandcamp = Bandcamp_df(df, catalog_info)
-    indexes = bandcamp.physical_indexes()
-    df = bandcamp.change_physical_item(indexes)
+def test_can_change_physical_item_functional(bandcamp_df):
+    indexes = bandcamp_df.physical_indexes()
+    df = bandcamp_df.change_physical_item(indexes)
     assert df.loc[1]['catalog number'].startswith('TR-00')
     assert type(df.loc[1]['item name']) == str
     assert type(df.loc[1]['artist']) == str
@@ -182,12 +112,4 @@ def test_can_change_physical_item_functional():
     assert type(df.loc[1]['upc']) == int
     assert df.loc[1]['item url'] == ''
 
-def test_can_make_catalog_entry_df():
-    catalog_df = load_catalog()
-    version_df = load_version()
-    catalog = Catalog(catalog_df, version_df, 3)
-    catalog_number = 'TR-001'
-    version_info = catalog.find_version_info(catalog_number)
-    assert len(version_info) > 0
-    assert version_info == ''
 
